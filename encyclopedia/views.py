b89877570b6c8b5e7fbce6d01ef67b10103b29db
Mayank Singh <singh13490mayank@gmail.com>
2020-12-04 13:25:14 +0530

    Added entry page and index page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+import markdown2
 from django.shortcuts import render
 
 from . import util
@@ -8,3 +9,18 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def get_page(request, title):
+    #get the contents of the page from util.py
+    page = util.get_entry(title)
+    #check if page exists or not
+    if page is None:
+        return render(request, "encyclopedia/error.html", {
+            "error": "The requested page was not found"
+        })
+    #display the contents of the page.
+    else:
+        return render(request, "encyclopedia/title.html", {
+            "title": title,
+            #convert markdown content to html
+            "content": markdown2.markdown(page)
+        })
